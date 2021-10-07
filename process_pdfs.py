b5af042dd6fab759694199119334eb740d0b5fe3
Bruno Benicio <brunobenicio2214@gmail.com>
2021-10-06 23:11:45 -0300

    create pdf processing utilities

---
+++ process_pdfs.py
@@ -0,0 +1,20 @@
+import pdf2image
+import glob
+import os
+
+def convert_pdfs(input_files=[], output_folder='./tmp', verbose=False):
+    def get_name(file_path):
+        return file_path.replace('\\', '/').split('/')[-1][:-4]
+
+    for file_path in input_files:
+        name = get_name(file_path)
+        if verbose: print('processing file:', name)
+        
+        output = os.path.join(output_folder, name)
+        os.makedirs(output, exist_ok=True)
+        
+        pdf2image.convert_from_path(file_path, output_folder=output, output_file='page', poppler_path='C:/Misc/poppler-21.09.0/Library/bin', fmt='png')
+        
+        if verbose:
+            out_files = [ get_name(f) for f in glob.glob(f'{output}/*.png') ]
+            print(f'\tconverted {len(out_files)} pages:', ','.join(out_files))
