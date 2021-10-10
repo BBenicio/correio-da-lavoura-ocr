a22e2458a27c81381c14ae25d7164733620abab1
Bruno Benicio <brunobenicio2214@gmail.com>
2021-10-10 17:50:28 -0300

    adjust workflow to use new cropping and evaluate quality

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import glob
-import cv2
 import os
 from unidecode import unidecode
+from tqdm import tqdm
 
 from process_pdfs import convert_pdfs
 from image_prep import prepare_image
-from image_processing import crop_margins, detect_columns
+from image_processing import crop_background, detect_columns
+from evaluate_quality import count_characters
 import utils
 
 PROCESS_PDFS = False
-VERBOSE = True
+VERBOSE = False
 
 def log(msg):
     if VERBOSE:
@@ -19,42 +20,42 @@ if PROCESS_PDFS:
     log('converting PDFs into PNGs')
     convert_pdfs(glob.glob('./input/raw/*.pdf'), './input/processed', VERBOSE)
 
+all_files = []
+
 editions = glob.glob('./input/processed/*')
 for ed in editions:
     ed_name = unidecode(utils.get_name(ed, 0).lower())
-    log(f'in edition "{ed_name}"')
     pages = glob.glob(f'{ed}/*.png')
     for page in pages:
         page_name = utils.get_name(page)
-        
-        log(f'...in page "{page_name}"')
-        image = utils.load_image(page)
-        
-        os.makedirs(f'./temp/{ed_name}/{page_name}/columns', exist_ok=True)
-        os.makedirs(f'./temp/{ed_name}/{page_name}/columns_temp', exist_ok=True)
-        os.makedirs(f'./output/{ed_name}/{page_name}/columns', exist_ok=True)
+        all_files.append((ed_name, page_name, page))
 
-        log('preparing image')
-        pre_image = prepare_image(image, f'./temp/{ed_name}/pre_{page_name}.png', f'./temp/{ed_name}/{page_name}/pre', verbose=VERBOSE)
-        
-        log('detecting the main body')
-        _, (x1, x2, y1, y2) = crop_margins(pre_image, f'./temp/{ed_name}/{page_name}/pre', f'./temp/{ed_name}/{page_name}/main.png')
+for ed_name, page_name, page in tqdm(all_files):
+    log(f'...in page "{page_name}" from "{ed_name}"')
+    image = utils.load_image(page)
+    
+    os.makedirs(f'./temp/{ed_name}/{page_name}/columns', exist_ok=True)
+    os.makedirs(f'./temp/{ed_name}/{page_name}/columns_temp', exist_ok=True)
+    os.makedirs(f'./output/{ed_name}/{page_name}/columns', exist_ok=True)
 
-        image = image[y1:y2, x1:x2]
-        log('preparing image')
-        image = prepare_image(image, f'./temp/{ed_name}/{page_name}.png', f'./temp/{ed_name}/{page_name}', verbose=VERBOSE)
+    log('cropping image')
+    image, _ = crop_background(image, f'./temp/{ed_name}/{page_name}/', f'./temp/{ed_name}/{page_name}/cropped.png')
 
-        log('detecting the columns')
-        detect_columns(image, f'./temp/{ed_name}/{page_name}/columns', f'./temp/{ed_name}/{page_name}/columns_temp', verbose=VERBOSE)
+    log('preparing image')
+    image = prepare_image(image, f'./temp/{ed_name}/{page_name}.png', f'./temp/{ed_name}/{page_name}', verbose=VERBOSE)
 
-        log('running OCR on the unprocessed page')
-        utils.run_ocr(page, f'./output/{ed_name}/{page_name}/base.txt', verbose=VERBOSE)
+    log('detecting the columns')
+    detect_columns(image, f'./temp/{ed_name}/{page_name}/columns', f'./temp/{ed_name}/{page_name}/columns_temp', verbose=VERBOSE)
 
-        log('running OCR on the grayscale page')
-        utils.run_ocr(f'./temp/{ed_name}/{page_name}/grayscale.png', f'./output/{ed_name}/{page_name}/gray.txt', verbose=VERBOSE)
+    log('running OCR on the unprocessed page')
+    utils.run_ocr(page, f'./output/{ed_name}/{page_name}/base.txt', verbose=VERBOSE)
 
-        log('running OCR on the processed columns')
-        utils.run_ocr_on_columns(glob.glob(f'./temp/{ed_name}/{page_name}/columns/*.png'), f'./temp/{ed_name}/{page_name}/columns', f'./output/{ed_name}/{page_name}/processed.txt')
+    log('running OCR on the grayscale page')
+    utils.run_ocr(f'./temp/{ed_name}/{page_name}/grayscale.png', f'./output/{ed_name}/{page_name}/gray.txt', verbose=VERBOSE)
 
-        log(f'DONE with page "{page_name}"')
-    log(f'DONE with edition "{ed_name}"')
+    log('running OCR on the processed columns')
+    utils.run_ocr_on_columns(glob.glob(f'./temp/{ed_name}/{page_name}/columns/*.png'), f'./temp/{ed_name}/{page_name}/columns', f'./output/{ed_name}/{page_name}/processed.txt')
+
+    log(f'DONE with page "{page_name}" from "{ed_name}"')
+
+count_characters()
