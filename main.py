0f86195dd4c41dcb07e382eff2ce0fcbaf0a061d
Bruno Benicio <brunobenicio2214@gmail.com>
2021-11-11 11:09:59 -0300

    insert mhs segmentation into workflow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import cv2
 from unidecode import unidecode
 from tqdm import tqdm
 import pandas as pd
@@ -8,9 +9,12 @@ from process_pdfs import convert_pdfs
 from image_prep import prepare_image
 from image_processing import crop_background, detect_columns
 from evaluate_quality import count_characters
+from mhs_layout_analisys import segment
 import utils
 
 PROCESS_PDFS = False
+DO_OCR = False
+DO_MHS = True
 VERBOSE = False
 
 def log(msg):
@@ -46,22 +50,27 @@ for ed_name, page_name, page in tqdm(all_files):
     log('preparing image')
     image = prepare_image(image, f'./temp/{ed_name}/{page_name}.png', f'./temp/{ed_name}/{page_name}', verbose=VERBOSE)
 
-    log('detecting the columns')
-    detect_columns(image, f'./temp/{ed_name}/{page_name}/columns', f'./temp/{ed_name}/{page_name}/columns_temp', verbose=VERBOSE)
+    if DO_OCR:
+        log('detecting the columns')
+        detect_columns(image, f'./temp/{ed_name}/{page_name}/columns', f'./temp/{ed_name}/{page_name}/columns_temp', verbose=VERBOSE)
 
-    log('running OCR on the unprocessed page')
-    _, base = utils.run_ocr(page, f'./output/{ed_name}/{page_name}/base.txt', verbose=VERBOSE)
+        log('running OCR on the unprocessed page')
+        _, base = utils.run_ocr(page, f'./output/{ed_name}/{page_name}/base.txt', verbose=VERBOSE)
 
-    log('running OCR on the grayscale page')
-    _, gray = utils.run_ocr(f'./temp/{ed_name}/{page_name}/grayscale.png', f'./output/{ed_name}/{page_name}/gray.txt', verbose=VERBOSE)
+        log('running OCR on the grayscale page')
+        _, gray = utils.run_ocr(f'./temp/{ed_name}/{page_name}/grayscale.png', f'./output/{ed_name}/{page_name}/gray.txt', verbose=VERBOSE)
 
-    log('running OCR on the processed page')
-    _, proc = utils.run_ocr(f'./temp/{ed_name}/{page_name}.png', f'./output/{ed_name}/{page_name}/proc.txt', verbose=VERBOSE)
+        log('running OCR on the processed page')
+        _, proc = utils.run_ocr(f'./temp/{ed_name}/{page_name}.png', f'./output/{ed_name}/{page_name}/proc.txt', verbose=VERBOSE)
 
-    log('running OCR on the columns')
-    _, cols = utils.run_ocr_on_columns(glob.glob(f'./temp/{ed_name}/{page_name}/columns/*.png'), f'./temp/{ed_name}/{page_name}/columns', f'./output/{ed_name}/{page_name}/processed.txt')
+        log('running OCR on the columns')
+        _, cols = utils.run_ocr_on_columns(glob.glob(f'./temp/{ed_name}/{page_name}/columns/*.png'), f'./temp/{ed_name}/{page_name}/columns', f'./output/{ed_name}/{page_name}/processed.txt')
 
-    confidence_scores.append({ 'edition': ed_name, 'page': page_name, 'base': base, 'grayscale': gray, 'columns': cols, 'processed': proc })
+        confidence_scores.append({ 'edition': ed_name, 'page': page_name, 'base': base, 'grayscale': gray, 'columns': cols, 'processed': proc })
+    
+    if DO_MHS:
+        inv = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+        segment(image, f'./temp/{ed_name}/{page_name}/')
 
     log(f'DONE with page "{page_name}" from "{ed_name}"')
 
